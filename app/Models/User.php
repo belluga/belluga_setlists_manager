740c9803965858e18ea318033f50ae126754e677
eltonmorais <elton@marketinglucrativo.com>
2024-07-06 13:49:13 -0300

    plain with mongo

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Wave\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -17,11 +19,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'username',
         'password',
-        'verification_code',
-        'verified',
-        'trial_ends_at',
     ];
 
     /**
@@ -35,11 +33,15 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * Get the attributes that should be cast.
      *
-     * @var array<string, string>
+     * @return array<string, string>
      */
-    protected $casts = [
-        'trial_ends_at' => 'datetime',
-    ];
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,57 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Wave\Facades\Wave;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
-     *
-     * @return void
+     * Seed the application's database.
      */
-    public function run()
+    public function run(): void
     {
-        $this->call(RolesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(UserRolesTableSeeder::class);
-        $this->call(AnnouncementsTableSeeder::class);
-        $this->call(ApiKeysTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(DataTypesTableSeeder::class);
-        $this->call(DataRowsTableSeeder::class);
-        $this->call(MenusTableSeeder::class);
-        $this->call(MenuItemsTableSeeder::class);
-        $this->call(NotificationsTableSeeder::class);
-        $this->call(PagesTableSeeder::class);
-        $this->call(PasswordResetsTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
-        $this->call(PermissionGroupsTableSeeder::class);
-        $this->call(PlansTableSeeder::class);
-        $this->call(PostsTableSeeder::class);
-        $this->call(SettingsTableSeeder::class);
-        $this->call(TranslationsTableSeeder::class);
-        $this->call(VoyagerThemesTableSeeder::class);
-        $this->call(VoyagerThemeOptionsTableSeeder::class);
-        $this->call(WaveKeyValuesTableSeeder::class);
-        fixPostgresSequence();
-    }
-}
+        // User::factory(10)->create();
 
-if (!function_exists('fixPostgresSequence')) {
-
-    function fixPostgresSequence()
-    {
-        if (config('database.default') === 'pgsql') {
-            $tables = \DB::select('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\' ORDER BY table_name;');
-            foreach ($tables as $table) {
-                if (\Schema::hasColumn($table->table_name, 'id')) {
-                    $seq = \DB::table($table->table_name)->max('id') + 1;
-                    \DB::select('SELECT setval(pg_get_serial_sequence(\'' . $table->table_name . '\', \'id\'), coalesce(' . $seq . ',1), false) FROM ' . $table->table_name);
-                }
-            }
-        }
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
