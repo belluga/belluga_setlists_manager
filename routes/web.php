4524ccbcd3be959adc59a1791fc396204dd8c33f
eltonmorais <elton@marketinglucrativo.com>
2024-07-07 17:18:21 -0300

    Revert "mary installed"
    This reverts commit 8578f917fe3008fdab936f33ecdb08e75b0d1c0f.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
-use Livewire\Volt\Volt;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\MusicController;
+use Illuminate\Support\Facades\Route;
 
-Volt::route('/', 'users.index');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/browse_musics', [MusicController::class, 'show']);
+Route::get('/browse_movies', [MovieController::class, 'show']);
