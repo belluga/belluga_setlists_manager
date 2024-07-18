366db38a40e54b92633062ff68f9868492618f79
eltonmorais <elton@marketinglucrativo.com>
2024-07-18 07:50:55 -0300

    clean unused imports - intalled chordsheetjs

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\MusicController;
-use App\Http\Controllers\SetlistController;
 use Illuminate\Support\Facades\Route;
 
 foreach (config('tenancy.central_domains') as $domain) {
